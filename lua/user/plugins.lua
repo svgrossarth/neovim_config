6938516185c91ec7d8d8a4f9502657622f1955d2
Spencer Grossarth <sgrossar@luna.io>
2022-07-19 15:22:57 -0700

    added diffview to diff files

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -91,6 +91,7 @@ return packer.startup(function(use)
 
   -- Git
   use { "lewis6991/gitsigns.nvim", commit = "c18e016864c92ecf9775abea1baaa161c28082c3" }
+  use { 'sindrets/diffview.nvim', requires = 'nvim-lua/plenary.nvim' }
 
   -- DAP
   use { "mfussenegger/nvim-dap", commit = "014ebd53612cfd42ac8c131e6cec7c194572f21d" }
